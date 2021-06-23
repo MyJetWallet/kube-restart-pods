ce5809a13a504c7bb05e314227df8c0355b7f4db
Alexey Novichikhin <32311883+NovichikhinAlexey@users.noreply.github.com>
2021-06-23 13:20:56 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-if [$NAMESPACE != 'null']
-then
 echo "restart in process ..."
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 RN=$(kubectl describe deployment $POD -n  $NAMESPACE|grep NewReplicaSet:|awk '{print $2}')
 PODS=$(for i in $(kubectl get pod -n $NAMESPACE| grep ${RN}|awk '{print $1}'); do echo $i; done)
+
+echo ${PODS}
+
 kubectl delete pod ${PODS} -n $NAMESPACE
 #kubectl get pods -n $NAMESPACE | grep $POD | cut -d " " -f1 | head -1 | xargs kubectl delete pod -n $NAMESPACE
-else
-echo "Restart is disables, namespace is null"
-fi
+
